afecd474beede093068d26bc48176a913b5ce133
TabWeng <hlwyfeng@gmail.com>
2016-06-01 08:34:12 +0800

    更新

--- xye/js/index.js
+++ xye/js/index.js
@@ -1,5 +1,20 @@
 $(function(){
 
+	var getMusic = document.getElementById('music');
+
+	$("#playMusic").click(function(){
+		if($(this).children().hasClass("glyphicon-play")){
+			$(this).children().attr("class","glyphicon glyphicon-pause");
+			$(this).css({"background-color":"#fff","color":"#ad8b3d"});
+			getMusic.play();
+		}else{
+			$(this).children().attr("class","glyphicon glyphicon-play");
+			$(this).css({"background-color":"#ad8b3d","color":"#fff"});
+			getMusic.pause();
+		}
+	});
+
+
 	var audio = document.getElementsByTagName('audio');
 
 	$(".ctrPlay").click(function(){
@@ -20,12 +35,17 @@ $(function(){
 			}
 
 			$(this).attr("class","glyphicon glyphicon-pause toPause");
+			
+			$("#playMusic").css({"background-color":"#fff","color":"#ad8b3d"});
+			$("#playMusic").children().attr("class","glyphicon glyphicon-play");
 			audio[getId].load();
 			audio[getId].play();
 			$(this).next().html("正在播放");
 
 		}else{
 			$(this).attr("class","glyphicon glyphicon-play toPlay");
+			$("#playMusic").children().attr("class","glyphicon glyphicon-pause");
+			$("#playMusic").css({"background-color":"#ad8b3d","color":"#fff"});
 			$(this).next().html("点击开始播放");
 			getThisId = $(this).attr("id");
 			audio[getThisId].pause();
